ceb5a7f2095157cf73ceb6398605fb8f65dcd134
Samir Ammar <samirammar.officially@gmail.com>
2024-08-20 21:20:13 +0400

    Dockerfile key and value new version

--- src/project/settings.py
+++ src/project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY', cast=str, default=None)
+SECRET_KEY = "django-insecure-^s3mzqryo(2ol&og4$xhd^w9ln$pnii6^47wai7-l55khfy7pq"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG', cast=bool)
